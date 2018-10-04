2e9d43b3c0fe2b1277a7b819286b0d843c100157
Alanikika Pratyaksa <alanikika26@gmail.com>
2018-10-04 14:30:20 +0700

    Fisrt Commit

---
+++ app/src/main/java/com/alanikika/pratyaksa/retreivingfromapi/api/ApiRepository.kt
@@ -0,0 +1,11 @@
+package com.alanikika.pratyaksa.retreivingfromapi.api
+
+import java.net.URL
+
+class ApiRepository {
+
+    fun doRequest(url: String): String {
+        return URL(url).readText()
+    }
+
+}
